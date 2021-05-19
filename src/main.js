0bf747e064c04127768ffecd86a5bd209150b3f2
jiayouzhao <jiayouzhaodaye@163.com>
2021-05-19 23:24:29 +0800

    fixed input bug

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+/* 
+ */
+let $searchInput = $(".searchInput");
 let x = JSON.parse(localStorage.getItem("x"));
 let sourceMap = x || [
 	{
@@ -6,22 +9,33 @@ let sourceMap = x || [
 		site:"https://www.acfun.cn"
 	},
 	{
-		logo:"B./image/bili.png",
+		logo:"B",
 		name:"bilibili.com",
 		site:"https://www.bilibili.com/"
 	}
 ];
 
 function render() {
-	sourceMap.forEach((item) => {
-		$(`<li>
+	$(".webSite").remove();
+	sourceMap.forEach((item, index) => {
+		let $li = $(`<li class="webSite">
             <a href="${item.site}">
                 <div class="siteList">
-                    <div class="top">${item["logo"][0]}</div>
+                    <div class="top"><i class="iconfont icon-${item.logo}"></i></div>
                     <div class="down">${item["name"]}</div>
                 </div>
             </a>
-        </li>`).insertBefore($(".addList"));
+            <div class="close">
+                <i class="iconfont icon-xx"></i>
+            </div>
+        </li>`);
+		$li.insertBefore($(".addList"));
+		$li.on("click", ".close", () => {
+			
+			sourceMap.splice(index, 1);
+			localStorage.setItem("x", JSON.stringify(sourceMap));
+			render();
+		});
 	});
 }
 
@@ -33,25 +47,55 @@ $(".addBtn").on("click", () => {
 	if (url === null) {
 		return; 
 	}
+	let regex = /^(((http|https):\/\/)?\w+\.\w+)$/gi;
+
+	if (!regex.test(url)) {
+		return alert("请输入正确网址");
+	}
 	if (url.length !== 0) {
-		let n = url.indexOf("http") + 1;
 		
-		if (!n) {
-			url = "https://" + url;
+		if (/^\s*\w+\.\w+$/.test(url)) {
+			obj.name = url.trim();
+			obj.site = "https://" + url.trim();
+			obj.logo = /[A-Z]/.test(obj.name[0].toLocaleUpperCase()) ? obj.name[0].toLocaleUpperCase() : "_img";
+		
+		} else if (/^(http|https):\/\/\w+\.\w+$/.test(url)) {
+			obj.site = url;
+			obj.name = url.replace(/(https|http):\/\//gi, "");
+			let initName = obj.name.trim()[0].toLocaleUpperCase();
+			obj.logo = /[A-Z]/.test(initName) ? initName : "_img";
 		}
-		obj.logo = url[0];
-		obj.name = url;
-		obj.site = url;
-		$(`<li>
-            <a href="${url}">
-                <div class="siteList">
-                    <div class="top">${url[0]}</div>
-                    <div class="down">${url}</div>
-                </div>
-            </a>
-        </li>`).insertBefore($(".addList"));
+
 		sourceMap.push(obj);
+		render();
+		
 		localStorage.setItem("x", JSON.stringify(sourceMap));
 	}
 	
+});
+
+$(".searchBtn").on("click", (e) => {
+	let $val = $(".searchInput").val();
+	if ($val.length === 0) {
+		e.preventDefault();
+		$searchInput.focus();
+	}
+});
+$(".searchInput").on("focus", (e) => {
+	e.stopPropagation();
+});
+$(document).keypress((e) => {
+	if (document.activeElement === $searchInput[0]) {
+		return; 
+	} else {
+		let { key } = e;
+		
+		sourceMap.forEach((item) => {
+            
+			if (item.logo.toLocaleLowerCase() === key) {
+				//console.log(item);
+				window.open(item.site);
+			}
+		});
+	}
 });
